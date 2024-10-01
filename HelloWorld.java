f47f8fc09f3a5efd8adb321cdbbc2255b526c5d0
JackLin0226 <jlin5@mtu.edu>
2024-10-01 15:57:13 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloJack {
+public class HelloWorld {
     public static void main(String[] args) {
         // Prints the message to the console
         System.out.println("Hello, World!");
